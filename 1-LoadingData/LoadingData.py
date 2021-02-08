dd7c8e72859dabbeeb27f01a8e949c9b2e4dc19f
Muhammad Syaiful <sebasmoran@icloud.com>
2021-02-08 16:59:30 +0700

    Adding prices prediction and array of images

--- 1-LoadingData/LoadingData.py
+++ 1-LoadingData/LoadingData.py
@@ -1,3 +1,20 @@
 from sklearn import datasets
+
 house_prices = datasets.load_boston()
+
+print("=======================")
+print("Output of Home Prices Data")
+print("=======================")
 print(house_prices.data)
+
+print("=======================")
+print("Output of Predicted Home Prices")
+print("=======================")
+print(house_prices.target)
+
+digits = datasets.load_digits()
+
+print("=======================")
+print("Output of scikit-learn Array of Images")
+print("=======================")
+print(digits.images[4])
